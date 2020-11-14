a4e5fba0231d1753b285017f880262cb3596e836
DavidZaro <dpulido@roadwego.com>
2020-11-14 13:25:00 -0600

    Respado DB

---
+++ db/Dump20201114.sql
@@ -0,0 +1,294 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: metlaguna
+-- ------------------------------------------------------
+-- Server version	5.7.24
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `comidatipica`
+--
+
+DROP TABLE IF EXISTS `comidatipica`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comidatipica` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(200) NOT NULL,
+  `historia` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comidatipica`
+--
+
+LOCK TABLES `comidatipica` WRITE;
+/*!40000 ALTER TABLE `comidatipica` DISABLE KEYS */;
+INSERT INTO `comidatipica` VALUES (1,'Gorditas','Echas en Gomez');
+/*!40000 ALTER TABLE `comidatipica` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cuenta`
+--
+
+DROP TABLE IF EXISTS `cuenta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cuenta` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `correo` varchar(100) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `validacion` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cuenta`
+--
+
+LOCK TABLES `cuenta` WRITE;
+/*!40000 ALTER TABLE `cuenta` DISABLE KEYS */;
+INSERT INTO `cuenta` VALUES (1,'prueba1@gmail.com','cfbd3e3a8adc49b9e0061ade86c84b499012048c903185821f9428dd981c4610b6d660d484a88641bf81b5b840422ea9e3a5da29c12821eed60b0d281e5f43db',_binary ''),(2,'prueba2@gmail.com','cfbd3e3a8adc49b9e0061ade86c84b499012048c903185821f9428dd981c4610b6d660d484a88641bf81b5b840422ea9e3a5da29c12821eed60b0d281e5f43db',NULL);
+/*!40000 ALTER TABLE `cuenta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `direccion`
+--
+
+DROP TABLE IF EXISTS `direccion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `direccion` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `lugar_id` int(11) DEFAULT NULL,
+  `calle` varchar(50) DEFAULT NULL,
+  `numero` int(11) DEFAULT NULL,
+  `colonia` varchar(50) DEFAULT NULL,
+  `codigoPostal` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `lugar_id` (`lugar_id`),
+  CONSTRAINT `direccion_ibfk_1` FOREIGN KEY (`lugar_id`) REFERENCES `lugar` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `direccion`
+--
+
+LOCK TABLES `direccion` WRITE;
+/*!40000 ALTER TABLE `direccion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `direccion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `foto`
+--
+
+DROP TABLE IF EXISTS `foto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `foto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `lugar_id` int(11) DEFAULT NULL,
+  `gastronomia_id` int(11) DEFAULT NULL,
+  `municipio_id` int(11) DEFAULT NULL,
+  `url` text,
+  `descripcion` varchar(200) DEFAULT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `tipo` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `lugar_id` (`lugar_id`),
+  KEY `gastronomia_id` (`gastronomia_id`),
+  KEY `municipio_id` (`municipio_id`),
+  CONSTRAINT `foto_ibfk_1` FOREIGN KEY (`lugar_id`) REFERENCES `lugar` (`id`),
+  CONSTRAINT `foto_ibfk_2` FOREIGN KEY (`gastronomia_id`) REFERENCES `gastronomia` (`id`),
+  CONSTRAINT `foto_ibfk_3` FOREIGN KEY (`municipio_id`) REFERENCES `municipio` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `foto`
+--
+
+LOCK TABLES `foto` WRITE;
+/*!40000 ALTER TABLE `foto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `foto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `gastronomia`
+--
+
+DROP TABLE IF EXISTS `gastronomia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `gastronomia` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `municipio_id` int(11) DEFAULT NULL,
+  `comidaTipica_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `municipio_id` (`municipio_id`),
+  KEY `comidaTipica_id` (`comidaTipica_id`),
+  CONSTRAINT `gastronomia_ibfk_1` FOREIGN KEY (`municipio_id`) REFERENCES `municipio` (`id`),
+  CONSTRAINT `gastronomia_ibfk_2` FOREIGN KEY (`comidaTipica_id`) REFERENCES `comidatipica` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gastronomia`
+--
+
+LOCK TABLES `gastronomia` WRITE;
+/*!40000 ALTER TABLE `gastronomia` DISABLE KEYS */;
+INSERT INTO `gastronomia` VALUES (1,1,1);
+/*!40000 ALTER TABLE `gastronomia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lugar`
+--
+
+DROP TABLE IF EXISTS `lugar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `lugar` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `municipio_id` int(11) DEFAULT NULL,
+  `tipoLugar_id` int(11) DEFAULT NULL,
+  `nombre` varchar(200) NOT NULL,
+  `descripcion` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `municipio_id` (`municipio_id`),
+  KEY `tipoLugar_id` (`tipoLugar_id`),
+  CONSTRAINT `lugar_ibfk_1` FOREIGN KEY (`municipio_id`) REFERENCES `municipio` (`id`),
+  CONSTRAINT `lugar_ibfk_2` FOREIGN KEY (`tipoLugar_id`) REFERENCES `tipolugar` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lugar`
+--
+
+LOCK TABLES `lugar` WRITE;
+/*!40000 ALTER TABLE `lugar` DISABLE KEYS */;
+/*!40000 ALTER TABLE `lugar` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `municipio`
+--
+
+DROP TABLE IF EXISTS `municipio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `municipio` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `estado` varchar(50) NOT NULL,
+  `descripcion` varchar(200) DEFAULT NULL,
+  `historia` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `municipio`
+--
+
+LOCK TABLES `municipio` WRITE;
+/*!40000 ALTER TABLE `municipio` DISABLE KEYS */;
+INSERT INTO `municipio` VALUES (1,'Gomez Palacio','Durango','Es la novena zona metropolitana más poblada de México con poco más de 1,400,000 habitantes.',''),(2,'Torreon','Coahuila','Es una de las ciudades más jóvenes de México, pues en 2007 celebró sus cien años.',''),(3,'Lerdo','Durango','Debe su nombre al ilustre liberal Miguel Lerdo de Tejada.',''),(4,'Otras Ciudades','Region Lagunera','Tlahualilo, Mapimí, Rodeo, Nazas, Simón Bolívar, San Juan de Guadalupe, San Luis del Cordero y San Pedro del Gallo, San Pedro, Matamoros, Francisco I. Madero y Viesca','');
+/*!40000 ALTER TABLE `municipio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tipolugar`
+--
+
+DROP TABLE IF EXISTS `tipolugar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tipolugar` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(200) NOT NULL,
+  `descripcion` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipolugar`
+--
+
+LOCK TABLES `tipolugar` WRITE;
+/*!40000 ALTER TABLE `tipolugar` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tipolugar` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombreUsuario` varchar(20) NOT NULL,
+  `nombres` varchar(100) NOT NULL,
+  `apellidoPaterno` varchar(50) NOT NULL,
+  `apellidoMaterno` varchar(50) NOT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `edad` int(11) DEFAULT NULL,
+  `foto` text,
+  `cuenta_id` int(11) DEFAULT NULL,
+  `ocupacion` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cuenta_id` (`cuenta_id`),
+  CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`cuenta_id`) REFERENCES `cuenta` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'TakitoTime','Prueba','TestPaterno','TestMaterno','8713975674',23,'WhatsApp Image 2020-09-29 at 9.22.39 PM.jpeg',1,'Programador');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'metlaguna'
+--
+
+--
+-- Dumping routines for database 'metlaguna'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-14 13:21:09
